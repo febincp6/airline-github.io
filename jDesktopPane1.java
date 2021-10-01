6aa170bb5cefe93fe3e9a9cfe37501a125fc3099
febincp6 <43105696+febincp6@users.noreply.github.com>
2021-10-01 13:47:22 +0530

    Add files via upload

---
+++ jDesktopPane1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+class jDesktopPane1 {
+
+    static class add {
+
+        public add() {
+        }
+    }
+    
+}
